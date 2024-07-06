f91d3b2b64848689261088a806a2fc9b741ded3f
Amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-07-06 18:05:28 +0330

    update to part 22

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, OtpCode
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
@@ -53,6 +53,7 @@ class UserRegisterForm(forms.Form):
         user = User.objects.filter(phone_number=phone_number).exists()
         if user:
             raise ValidationError('Phone number already registered')
+        OtpCode.objects.filter(phone_number=phone_number).delete()
         return phone_number
 
 
--- accounts/management/commands/remove_expired_otps.py
+++ accounts/management/commands/remove_expired_otps.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
 from accounts.models import OtpCode
-from datetime import datetime, timedelta
+from datetime import timedelta, datetime
+from django.utils import timezone
+import pytz
 
 
 class Command(BaseCommand):
     help = 'Remove expired otps'
 
     def handle(self, *args, **options):
-        expired_time = datetime.now() - timedelta(minutes=2)
+        expired_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
         OtpCode.objects.filter(created__lt=expired_time).delete()
         self.stdout.write(self.style.SUCCESS('Successfully removed expired otps'))
