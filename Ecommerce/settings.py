92ceeb6549306b26942d06436b5ae3d33fcce1f2
Amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-07-06 23:45:15 +0330

    update to part 23(celery are not work!(must be repair))

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
-    'storages',
+    # 'storages',
+    'django_celery_beat',
+
 ]
 
 MIDDLEWARE = [
