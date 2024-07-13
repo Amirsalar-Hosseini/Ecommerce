6e8ed45494d4246091d0a6eb96f09130b79a780d
Amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-07-13 21:04:54 +0330

    update to part 38(payment was not complte!)

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from home.models import Product
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.IntegerField(blank=True, null=True, default=None)
+
+    class Meta:
+        ordering = ['paid', '-updated']
+
+    def __str__(self):
+        return f'{self.user} - {str(self.id)}'
+
+    def get_total_price(self):
+        total = sum(item.get_cost() for item in self.items.all())
+        if self.discount:
+            discount_price = total * (self.discount / 100)
+            return int(total - discount_price)
+        return total
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{str(self.id)}'
+
+    def get_cost(self):
+        return self.price * self.quantity
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(99)])
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.code
--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .cart import Cart
 from home.models import Product
-from .forms import CartAddForm
+from .forms import CartAddForm, CouponApplyForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order, OrderItem, Coupon
+from django.conf import settings
+import requests
+import json
+import datetime
+from django.contrib import messages
 
 
 class CartView(View):
@@ -27,3 +34,108 @@ class CartRemoveView(View):
         product = get_object_or_404(Product, id=product_id)
         cart.remove(product)
         return redirect('orders:cart')
+
+
+class OrderDetailView(LoginRequiredMixin, View):
+    form_class = CouponApplyForm
+
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        return render(request, 'orders/order.html', {'order': order, 'form': self.form_class})
+
+
+class OrderCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        cart = Cart(request)
+        order = Order.objects.create(user=request.user)
+        for item in cart:
+            OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+        cart.clear()
+        return redirect('orders:order_detail', order.id)
+
+
+# if settings.SANDBOX:
+#     sandbox = 'sandbox'
+# else:
+#     sandbox = 'www'
+#
+# ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+# ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+# ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+# CallbackURL = 'http://127.0.0.1:8000/order/verify/'
+# description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"
+
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        request.session['order_pay'] = {
+            'order_id': order.id
+        }
+        # data = {
+        #     "MerchantID": settings.MERCHANT,
+        #     "Amount": order.get_total_price(),
+        #     "Description": description,
+        #     "Phone": request.user.phone_number,
+        #     "CallbackURL": CallbackURL,
+        # }
+        # data = json.dumps(data)
+        # headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+        # try:
+        #     response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+        #
+        #     if response.status_code == 200:
+        #         response = response.json()
+        #         if response['Status'] == 100:
+        #             return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
+        #                     'authority': response['Authority']}
+        #         else:
+        #             return {'status': False, 'code': str(response['Status'])}
+        #     return response
+        #
+        # except requests.exceptions.Timeout:
+        #     return {'status': False, 'code': 'timeout'}
+        # except requests.exceptions.ConnectionError:
+        #     return {'status': False, 'code': 'connection error'}
+
+
+class OrderVerifyView(LoginRequiredMixin, View):
+    def get(self, request):
+        order_id = request.session['order_pay']['order_id']
+        order = Order.objects.get(id=int(order_id))
+        # data = {
+        #     "MerchantID": settings.MERCHANT,
+        #     "Amount": order.get_total_price(),
+        #     "Authority": request.GET['Authority'],
+        # }
+        # data = json.dumps(data)
+        # # set content length by data
+        # headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+        # response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
+        #
+        # if response.status_code == 200:
+        #     response = response.json()
+        #     if response['Status'] == 100:
+        #         return {'status': True, 'RefID': response['RefID']}
+        #     else:
+        #         return {'status': False, 'code': str(response['Status'])}
+        # return response
+
+
+class CouponApplyView(LoginRequiredMixin, View):
+    form_class = CouponApplyForm
+
+    def post(self, request, order_id):
+        now = datetime.datetime.now()
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            try:
+                coupon = Coupon.objects.get(code__exact=code, valid_from__lte=now, valid_to__gte=now, active=True)
+            except Coupon.DoesNotExist:
+                messages.error(request, 'Coupon does not exist', 'danger')
+                return redirect('orders:order_detail', order_id)
+            order = Order.objects.get(id=order_id)
+            order.discount = coupon.discount
+            order.save()
+            return redirect('orders:order_detail', order.id)
